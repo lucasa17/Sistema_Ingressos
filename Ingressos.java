c086cd989a1cb59c0984a97e7377982c18f8500b
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-03-31 07:27:00 -0300

    Create Ingressos.java

---
+++ Ingressos.java
@@ -0,0 +1,113 @@
+package ingressos;
+
+import java.util.Scanner;
+
+public class Ingressos {
+
+    public static void main(String[] args) {
+        SistemaIngressos sistema = new SistemaIngressos();
+        Scanner scanner = new Scanner(System.in);
+        String nomeArquivoPessoas = "pessoas.txt";
+        String nomeArquivoEventos = "eventos.txt";
+
+
+        // Carregar dados do arquivo
+        sistema.carregarDadosPessoas(nomeArquivoPessoas);
+        sistema.carregarDadosEventos(nomeArquivoEventos);
+
+
+        while (true) {
+            System.out.println("\n\n1. Cadastrar Pessoa");
+            System.out.println("2. Listar Pessoas");
+            System.out.println("3. Remover Pessoa");
+            System.out.println("4. Cadastrar Evento");
+            System.out.println("5. Atualizar Evento");
+            System.out.println("6. Lista Eventos");
+            System.out.println("7. Remover Evento");
+            System.out.println("8. Comprar Ingressos");
+            System.out.println("9. Salvar e Sair");
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Nome: ");
+                    String nome = scanner.nextLine();
+                    System.out.print("Idade: ");
+                    int idade = scanner.nextInt();
+                    scanner.nextLine(); // Limpar o buffer
+                    System.out.print("Sexo (M/F): ");
+                    char sexo = scanner.nextLine().charAt(0);
+                    sistema.adicionarPessoa(new Pessoa(nome, idade, sexo, "."));
+                    break;
+
+                case 2:
+                    System.out.println("Lista de Pessoas:");
+                    sistema.listarPessoas();
+                    break;
+
+                case 3:
+                    System.out.print("Nome da Pessoa a remover: ");
+                    String nomeRemover = scanner.nextLine();
+                    sistema.removerPessoa(nomeRemover);
+                    break;
+
+                case 4:
+                    System.out.print("Nome do Evento: ");
+                    String nomeEvento = scanner.nextLine();
+                    System.out.print("Data do Evento: ");
+                    String dataEvento = scanner.nextLine();
+                    System.out.print("Ingressos Disponíveis: ");
+                    int ingressosDisponiveis = scanner.nextInt();
+                    sistema.adicionarEvento(new Evento(nomeEvento, dataEvento, ingressosDisponiveis));
+                break;
+                
+                case 5:
+                    
+                    sistema.listarEventos();
+
+                    System.out.print("\nNome do evento a atualizar: ");
+                    String nomeAtualizar = scanner.nextLine();
+                    System.out.print("Novo Nome: ");
+                    String novoNome = scanner.nextLine();
+                    System.out.print("Nova data: ");
+                    String novaData = scanner.nextLine();
+                    scanner.nextLine(); // Limpar o buffer
+                    System.out.print("Nova quantia de ingressos: ");
+                    int novaQuantia = scanner.nextInt();
+                    sistema.atualizarEvento(nomeAtualizar, new Evento(novoNome, novaData, novaQuantia));
+                    break;
+
+                case 6:
+                    System.out.println("Lista de Eventos:");
+                    sistema.listarEventos();
+                    break;
+                
+                case 7:
+                    System.out.print("Nome do evento a remover: ");
+                    String nomeEventoRemove = scanner.nextLine();
+                    sistema.removerEvento(nomeEventoRemove);
+                break;
+                    
+                case 8:
+                    System.out.print("Nome do Evento para comprar ingressos: ");
+                    String eventoComprar = scanner.nextLine();
+                    System.out.print("Quantidade de ingressos: ");
+                    int quantidade = scanner.nextInt();
+                    sistema.comprarIngresso(eventoComprar, quantidade);
+                    break;
+
+                case 9:
+                    sistema.salvarDadosPessoas(nomeArquivoPessoas);
+                    sistema.salvarDadosEventos(nomeArquivoEventos);
+                    System.out.println("Saindo do sistema...");
+                    scanner.close();
+                    return; // Encerra o programa
+
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        }
+    }
+}
